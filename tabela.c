06830ca8b4367abba629d599339a3964836f0577
slackman <rodrigomsc@gmail.com>
2009-06-10 20:35:34 -0300

    Continuando a implementacao da declaracao de tipos

--- tabela.c
+++ tabela.c
@@ -4,7 +4,7 @@
 /* Procura na tabela de simbolos, nela iremos colocar nossas palavras reservadas e variaveis que estao sendo criadas */
 /* se a palavra nao for encontrada ela e adicionada a tabela de simbolos*/
 tabelaSimb* achaOuAdicionaId(char *nome){
-        tabelaSimb *sp;
+        tabelaSimb *sp = NULL;
         int count;
 
 	for(sp = tabSimb, count = 0; sp < &tabSimb[MAX_SIMB]; sp++, count++){ 
@@ -22,13 +22,13 @@ tabelaSimb* achaOuAdicionaId(char *nome){
 
 	//a tabela de simbolos tem uma quantidade max, cuidado para n estourar
 	yyerror("Espaco insuficiente.\n");
-	return 0;
+        return sp;
 }
 
 /* Procura na tabela de simbolos, nela iremos colocar nossas palavras reservadas e variaveis que estao sendo criadas */
 /* se a palavra nao for encontrada ela e adicionada a tabela de simbolos*/
 tabelaSimb* achaId(char *nome){
-        tabelaSimb *sp;
+        tabelaSimb *sp = NULL;
         int count;
 
 	for(sp = tabSimb, count = 0; sp < &tabSimb[MAX_SIMB]; sp++, count++){ 
@@ -39,5 +39,5 @@ tabelaSimb* achaId(char *nome){
 
 	//a tabela de simbolos tem uma quantidade max, cuidado para n estourar
 	yyerror("ID nao encontrado.\n");
-	return 0;
+        return sp;
 }
